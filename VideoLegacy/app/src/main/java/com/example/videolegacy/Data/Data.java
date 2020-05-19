c49c0a088dbbdaacc3aba9a09840fc3d66a9ea52
Bryan Garro Eduarte <bryangarroeduarte@hotmail.com>
2020-05-18 22:10:11 -0600

    onclick

--- VideoLegacy/app/src/main/java/com/example/videolegacy/Data/Data.java
+++ VideoLegacy/app/src/main/java/com/example/videolegacy/Data/Data.java
@@ -1,15 +1,22 @@
 package com.example.videolegacy.Data;
 
 import com.example.videolegacy.Logica.Movie;
+import com.example.videolegacy.Logica.Serie;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Data {
     private List<Movie> movieList;
+    private List<Serie> horrorSeries;
+
     public Data(){
         movieList = new ArrayList<>();
+        horrorSeries = new ArrayList<>();
         this.prepareMovies();
+        this.prepareHorrorSeries();
+
+
     }
     public void prepareMovies(){
         Movie movie = new Movie("The Avengers", "Action", 2015);
@@ -23,8 +30,58 @@ public class Data {
         Movie movie4 = new Movie("La Llorona", "Terror", 2020);
         this.movieList.add(movie4);
     }
+    public void prepareHorrorSeries(){
+        Serie serie = new Serie("American Horror Story", "Historias de terror", 2011);
+        this.horrorSeries.add(serie);
+         serie = new Serie("hill house", "La historia de una maldicion", 2018);
+        this.horrorSeries.add(serie);
+         serie = new Serie("Stranger Things", "Sucesos paranormales", 2016);
+        this.horrorSeries.add(serie);
+         serie = new Serie("Marianne", "La bruja maldita", 2019);
+        this.horrorSeries.add(serie);
+         serie = new Serie("Slasher", "El espiritu asesino", 2016);
+        this.horrorSeries.add(serie);
+        serie = new Serie("Haunted", "Historia de un asesino que caza a sus presas", 2018);
+        this.horrorSeries.add(serie);
+        serie = new Serie("Kingdom", "Fantasmas asesinos", 2017);
+        this.horrorSeries.add(serie);
+        serie = new Serie("Supernatural", "Eventos inexplicables", 2005);
+        this.horrorSeries.add(serie);
+        serie = new Serie("Chambers", "Camaras de tortura", 2019);
+        this.horrorSeries.add(serie);
+        serie = new Serie("iZombie", "apocalipsis zombie", 2015);
+        this.horrorSeries.add(serie);
+        serie = new Serie("Dracula", "El vampiro", 2016);
+        this.horrorSeries.add(serie);
+        serie = new Serie("True blood", "Vampiros sofisticados y  mafiosos", 2016);
+        this.horrorSeries.add(serie);
+        serie = new Serie("outcast", "la Maldicion", 2016);
+        this.horrorSeries.add(serie);
+        serie = new Serie("Hannibal", "El come hombres", 2016);
+        this.horrorSeries.add(serie);
+        serie = new Serie("Fear itself", "miedo a tus propios demonios", 2016);
+        this.horrorSeries.add(serie);
 
+    }
+    public String[] horrorSeriesToArray(){
+        ArrayList<String> horrorSeriesArray = new ArrayList<>();
+
+        for (Serie serie:this.horrorSeries) {
+            horrorSeriesArray.add(serie.getTitulo());
+        }
+        String[] simpleArray = new String[ horrorSeriesArray.size() ];
+        horrorSeriesArray.toArray( simpleArray );
+        return simpleArray;
+    }
+    public Serie getSerieByTitulo(String titulo){
+        for (Serie serie:this.horrorSeries) {
+            if(serie.getTitulo().equals(titulo))
+                return serie;
+        }
+        return null;
+    }
     public List<Movie> getMovieList() {
         return movieList;
     }
+
 }
--- VideoLegacy/app/src/main/java/com/example/videolegacy/Fragments/Horror.java
+++ VideoLegacy/app/src/main/java/com/example/videolegacy/Fragments/Horror.java
@@ -13,16 +13,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridLayout;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import com.example.videolegacy.Adapter.HorrorAdapter;
+import com.example.videolegacy.Data.Data;
 import com.example.videolegacy.R;
 
 
 public class Horror extends Fragment implements HorrorAdapter.ItemClickListener{
-    String[] horrorFilms = {"The Witcher","Lost","Rick and Morty","Arrow",
-            "The Flash","Titans", "Batlefish", "Archer", "Final Space", "Supergirl", "Marco Polo","Sherlock",
-            "Shooter", "Merlin", "Star Trek", "Castlevania", "Black Sails", "Hitman"};
+    String[] horrorFilms ;
     HorrorAdapter adapter;
+    Data db;
     public Horror() {
         // Required empty public constructor
     }
@@ -34,6 +35,8 @@ public class Horror extends Fragment implements HorrorAdapter.ItemClickListener{
         //RecyclerView recyclerView = findViewById(R.id.rvNumbers);
         View view =  inflater.inflate(R.layout.fragment_horror, container, false);
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.rvNumbers);
+        db = new Data();
+        horrorFilms = db.horrorSeriesToArray();
         int numberOfColumns = 3;
         recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), numberOfColumns));
         adapter = new HorrorAdapter(getActivity(), horrorFilms);
@@ -47,6 +50,8 @@ public class Horror extends Fragment implements HorrorAdapter.ItemClickListener{
         Log.i("TAG", "You clicked number " + adapter.getItem(position) + ", which is at cell position " + position);
 
     }
+
+
 
 
 }
